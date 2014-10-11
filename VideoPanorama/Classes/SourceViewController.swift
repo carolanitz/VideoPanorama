6242a6fd91bfc56dbc928b1588596e252cf649ea
Gernot Poetsch <github@poetsch.org>
2014-10-11 12:07:12 +0200

    Added a Source ViewController and Storyboard

---
+++ VideoPanorama/Classes/SourceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SourceViewController.swift
+//  VideoPanorama1
+//
+//  Created by Gernot Poetsch on 11.10.14.
+//
+//
+
+import UIKit
+
+class SourceViewController: UIViewController {
+
+
+
+}
